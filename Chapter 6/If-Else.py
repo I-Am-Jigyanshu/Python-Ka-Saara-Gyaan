2fdbde3208a009dc845b21bc74fcf378178423b6
Jigyanshu <68092947+I-Am-Jigyanshu@users.noreply.github.com>
2022-03-10 11:09:38 +0530

    Add files via upload

---
+++ Chapter 6/If-Else.py
@@ -0,0 +1,15 @@
+'''
+var1 = 6
+var2 = 56
+print("Tell me the number: ")
+var3 = int(input())
+if var3>var2:
+    print("Greater")
+elif var3==var2:
+    print("It is Equal")
+else:
+    print("Lesser")
+    '''
+list = [5,7,3]
+if 5 in list:
+    print("Yes it is in the list")
---
+++ Chapter 6/Quiz.py
@@ -0,0 +1,11 @@
+
+print("Enter your Age: ")
+Age = int(input())
+if Age>18 and Age<80:
+    print("You are Legal to Drive")
+elif Age == 18:
+    print("You can ???")
+elif Age<7 or Age>80:
+    print("Chutiya hogye ho ka")
+else:
+    print("You are not legal to drive..")
